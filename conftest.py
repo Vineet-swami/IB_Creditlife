0887f4f05961787128b997aab52dac467d2c90f3
Vineet Swami <vineetswami@LP-000876s-MacBook-Pro.local>
2026-01-05 01:26:19 +0530

    CI changes In config file

--- conftest.py
+++ conftest.py
@@ -1,4 +1,5 @@
 import pytest_asyncio
+import os
 from playwright.async_api import async_playwright
 from dotenv import load_dotenv
 
@@ -7,8 +8,15 @@ load_dotenv()
 @pytest_asyncio.fixture
 async def page():
     async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=True, args=["--no-sandbox"])
-        page = await browser.new_page()
+        is_ci = os.getenv("CI") == "true"
+        headless = os.getenv("HEADLESS", "true").lower() == "true" if not is_ci else True
+        browser = await p.chromium.launch(headless=headless, args=["--no-sandbox"])
+        context = await browser.new_context()
+        page = await context.new_page()
         yield page
-        input("Press ENTER to close the browser...")
+        if not is_ci:
+            input("Press ENTER to close the browser...")
+
+        await context.close()
+        await browser.close()
 
